559ca1c5db7286f2099777f64889f1e8be7f3d7c
Ryan <day.ryanm@gmail.com>
2018-11-27 23:59:40 -0800

    more files

---
+++ C# Practice/Small Projects/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
+        }
+    }
+}
---
+++ C# Practice/Small Projects/HelloWorld/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+                Console.WriteLine("Welcome to Acme Accounting Systems");
+                Console.WriteLine("Remember, we're \"accounting\" on you!");
+                Console.ReadLine();
+        }
+    }
+}
