0260ab4b14f7e967eb40fbea069b7cd56c61aad8
Ryan <day.ryanm@gmail.com>
2018-11-30 11:30:03 -0800

    new projects to review

---
+++ C# Practice/Small Projects/MathAndComparison/MathAndComparison/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int comboTotal = total + otherTotal;
+            //Console.WriteLine("the combination total is" + comboTotal.ToString());
+            ////Console.ReadLine();
+
+            //int remainder = 10 % 3;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            int roomTemperature = 70;
+            int currTemp = 70;
+
+
+            bool isWarm = currTemp >= roomTemperature;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ C# Practice/Small Projects/drill66/drill66/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drill66
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Pick a number, any number...");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+            int output1 = userNum1 * 50;
+            Console.WriteLine(output1);
+            
+            Console.WriteLine("Pick a number, any number...");
+            int userNum2 = Convert.ToInt32(Console.ReadLine());
+            int output2 = userNum2 + 25;
+            Console.WriteLine(output2);
+            
+            Console.WriteLine("Pick a number, any number...");
+            double userNum3 = Convert.ToDouble(Console.ReadLine());
+            double output3 = userNum3 / 12.5;
+            Console.WriteLine(output3);
+            
+            Console.WriteLine("Pick a number, any number...");
+            int userNum4 = Convert.ToInt32(Console.ReadLine());
+            bool output4 = userNum4 > 50;
+            Console.WriteLine(output4);
+            
+            Console.WriteLine("Pick a number, any number...");
+            int userNum5 = Convert.ToInt32(Console.ReadLine());
+            int output5 = userNum5 % 7;
+            Console.WriteLine(output5);
+            Console.ReadLine();
+
+
+        }
+    }
+}
