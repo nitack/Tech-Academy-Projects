501deca4f621e54d1828e56480ff46ea77fd4355
Ryan <day.ryanm@gmail.com>
2018-11-29 17:20:17 -0800

    more projects

---
+++ C# Practice/Small Projects/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string pageNum = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything?  Please answer \"true\" or \"false\"");
+            string needHelp = Console.ReadLine();
+            Console.WriteLine("Where there any positive experiences you'd like to share?  Please give specifics.");
+            string posExperience = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide?  Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string studyHours = Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
