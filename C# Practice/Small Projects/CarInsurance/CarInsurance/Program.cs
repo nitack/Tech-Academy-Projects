0e2ed7a7605a6cd79aea3a299c8c6323385042e1
Ryan <day.ryanm@gmail.com>
2018-12-02 21:04:36 -0800

    drill 75

---
+++ C# Practice/Small Projects/CarInsurance/CarInsurance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            Console.WriteLine((age > 15) && (dui == false) && (tickets <= 3));
+            Console.ReadLine();
+        }
+    }
+}
