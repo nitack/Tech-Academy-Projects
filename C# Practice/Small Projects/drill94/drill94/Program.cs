b1774c35141e68b82e22555f880994db98441943
Ryan <day.ryanm@gmail.com>
2018-12-03 17:46:20 -0800

    drill94

---
+++ C# Practice/Small Projects/drill94/drill94/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drill94
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Guess my weight...");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = weight == 235;
+
+            do
+            {
+                switch (weight)
+                {
+                    case 0:
+                        Console.WriteLine("Zero... Really?");
+                        Console.WriteLine("Guess again...");
+                        weight = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 1:
+                        Console.WriteLine("One... Really?");
+                        Console.WriteLine("Guess again...");
+                        weight = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 235:
+                        Console.WriteLine("You are correct, 235 pounds.");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Not quite... try again.");
+                        Console.WriteLine("Guess again...");
+                        weight = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+            Console.ReadLine();
+        }
+    }
+}
