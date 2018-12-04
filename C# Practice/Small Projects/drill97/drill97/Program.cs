32bf43665883af2b4650cd5b610f77b96095c3d6
Ryan <day.ryanm@gmail.com>
2018-12-03 19:40:56 -0800

    drill97

---
+++ C# Practice/Small Projects/drill97/drill97/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drill97
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Pick a word...");
+            string word1 = Console.ReadLine();
+            Console.WriteLine("Pick another word...");
+            string word2 = Console.ReadLine();
+            Console.WriteLine("Pick one more word...");
+            string word3 = Console.ReadLine();
+            Console.WriteLine(word1 + " " + word2 + " " + word3);
+            string uword = word1.ToUpper();
+            Console.WriteLine(uword);
+            StringBuilder sb = new StringBuilder("");
+            Console.WriteLine("Write one sentence:");
+            sb.Append(Console.ReadLine());
+            Console.WriteLine("Write another sentence:");
+            sb.Append(" " + Console.ReadLine());
+            Console.WriteLine("Write one more sentence:");
+            sb.Append(" " + Console.ReadLine());
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
