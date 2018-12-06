f9acff1228705545dcc27d80b9238151fce373c7
Ryan <day.ryanm@gmail.com>
2018-12-05 19:37:58 -0800

    drill100

---
+++ C# Practice/Small Projects/ArraysandLists/ArraysandLists/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        //List<int> intList = new List<int>();
+        //intList.Add(4);
+        //intList.Add(10);
+        ////intList.Remove(10);
+
+        //Console.WriteLine(intList[1]);
+        //Console.ReadLine();
+        
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        //numArray2[5] = 650;
+
+        //Console.WriteLine(numArray2[5]);
+        //Console.ReadLine();
+    }
+}
+
---
+++ C# Practice/Small Projects/drill100/drill100/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace drill100
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string[] stringArray = new string[] { "index Zero", "index One", "index Two", "index Three", "index Four"};
+            Console.WriteLine("Choose an index number between 0 and 4...");
+            int userString = Convert.ToInt16(Console.ReadLine());
+            if (userString >= 0 && userString <= 4)
+            {
+                Console.WriteLine(stringArray[userString]);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("That index number is not between 0 and 4.");
+                Console.ReadLine();
+            }
+
+            int[] intArray = new int[] { 2, 4, 6, 8, 10 };
+            Console.WriteLine("Choose an index number between 0 and 4...");
+            int userInt = Convert.ToInt16(Console.ReadLine());
+            if (userInt >= 0 && userInt <= 4)
+            {
+                Console.WriteLine(intArray[userInt]);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("That index number is not between 0 and 4.");
+                Console.ReadLine();
+            }
+
+            List<string> stringList = new List<string>();
+            stringList.Add("First index in the list");
+            stringList.Add("Second index in the list");
+            stringList.Add("Third index in the list");
+            stringList.Add("Fourth index in the list");
+            stringList.Add("Fifth index in the list");
+
+            Console.WriteLine("Choose an index number between 0 and 4...");
+            int userListNum = Convert.ToInt16(Console.ReadLine());
+            if (userListNum >= 0 && userListNum <= 4)
+            {
+                Console.WriteLine(stringList[userListNum]);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("That index number is not between 0 and 4.");
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
