24043b6ddb1794014d3ae9a92ab4871190ac16a0
Ryan <day.ryanm@gmail.com>
2018-12-24 10:18:58 -0800

    drill115

---
+++ C# Practice/Small Projects/drill115/drill115/Maths.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drill115
+{
+    class Maths
+    {
+        public static int Addition(int input, int optionalInput = 3)
+        {
+            return(input + optionalInput);
+        }
+    }
+}
---
+++ C# Practice/Small Projects/drill115/drill115/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1. Create a class. In that class, create a method that takes two integers as parameters.
+//   Make one of them optional.Have the method do a math operation and return a integer result.
+
+//2. In the Main() method of the console app, instantiate the class.
+
+//3. Ask the user to input two numbers, one at a time.Let them know they need not enter 
+//   anything for the second number.
+
+//4. Call the method in the class, passing in the one or two numbers entered.
+
+//5. Try various combinations of numbers on the code, including having no second number.
+
+namespace drill115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maths opps = new Maths();
+
+            Console.WriteLine("Pick a number");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick another number, or don't...");
+            int userNum2 = Convert.ToInt32(Console.ReadLine());
+            int resultAdd = Maths.Addition(userNum1, userNum2);
+            Console.WriteLine(resultAdd);
+            Console.ReadLine();
+        }
+    }
+}
